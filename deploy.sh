0815a49fdce782e97414198329a8e26ebfcf05c3
Lucas Garron <code@garron.net>
2014-06-20 07:10:02 -0700

    Cleaned up cache.manifest updating in deploy script.

--- deploy.sh
+++ deploy.sh
@@ -4,18 +4,20 @@
 # This script modifies the manifest file to include a unique timestamp on every upload (so that browsers will look for a new version). In case of failure, we end up with a modified manifest file that will be restored to normal on next success.
 MANIFEST_FILE="inc/cache.manifest"
 
+# Set this to ".bak" if you want to protect your cache manifest in case `sed` fails.
+BACKUP_EXTENSION=".bak"
+
 # Call this before deploying to your web server.
 function pre_deploy {
-	DATE="`date`"
-	echo "Updating cache manifest to: ${DATE}"
-	NEW_CACHE_MANIFEST=`cat "${MANIFEST_FILE}" | sed "s/Uploaded on .*/Uploaded on ${DATE}/"`
-	echo "${NEW_CACHE_MANIFEST}" > "${MANIFEST_FILE}"
+  DATE="$(date)"
+  echo "[CACHE MANIFEST] Creating unique version by updating date to: ${DATE}"
+  sed -i "${BACKUP_EXTENSION}" "s/Uploaded on .*/Uploaded on ${DATE}/" "${MANIFEST_FILE}"
 }
 
 # Call this after deploying to your web server to restore the manifest file.
 function post_deploy {
-	NEW_CACHE_MANIFEST=`cat "${MANIFEST_FILE}" | sed "s/Uploaded on .*/Uploaded on [DATE]/"`
-	echo "${NEW_CACHE_MANIFEST}" > "${MANIFEST_FILE}"
+  echo "[CACHE MANIFEST] Reverting."
+  sed -i "${BACKUP_EXTENSION}" "s/Uploaded on .*/Uploaded on [DATE]/" "${MANIFEST_FILE}"
 }
 
 
